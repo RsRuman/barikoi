f9ebd2c44fc35f8595f60c03be9e613253c461d9
RumanDT <ruman@decentthemes.com>
2024-08-01 16:38:00 +0600

    install broadcasting and setup redis and reverb

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (!Cache::has("users")) {
+        Cache::remember('users', 3600, function () {
+            return \App\Models\User::all();
+        });
+    }
+
+    $user = Cache::get('users');
+
+    \App\Events\UserBroadcastEvent::dispatch($user);
+
+    echo 'data serve using reverb broadcasting';
 });
